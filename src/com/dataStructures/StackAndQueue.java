9f02949431fe6a39b96bf0de6285e9640ce64045
unknown <khomanesuraj8@gmail.com>
2021-10-25 16:31:31 +0530

    create stack and push element

--- src/com/dataStructures/StackAndQueue.java
+++ src/com/dataStructures/StackAndQueue.java
@@ -1,10 +1,14 @@
 package com.dataStructures;
 
 public class StackAndQueue<E> {
+	//variable declaration
 	Node<E> head;
 
+	//method: push element
 	public void push(E key) {
+		//create new node
 		Node<E> newNode = new Node<E>(key);
+		//check stack is empty or not
 		if (isEmpty()) {
 			head = newNode;
 			return;
@@ -15,10 +19,13 @@ public class StackAndQueue<E> {
 		}
 		temp.next = newNode;
 	}
-
+	
+	//check stack is empty or not
 	public boolean isEmpty() {
 		return head == null;
 	}
+	
+	//method: print stack and queue
 	public void print() {
 		Node<E> temp = head;
 		while(temp != null) {
