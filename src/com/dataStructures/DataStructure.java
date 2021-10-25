446ab682acfc45ca213c7652fa16653b2e39cd0f
unknown <khomanesuraj8@gmail.com>
2021-10-25 17:36:25 +0530

    Added peek and pop method

--- src/com/dataStructures/DataStructure.java
+++ src/com/dataStructures/DataStructure.java
@@ -1,22 +1,49 @@
 package com.dataStructures;
 
 public class DataStructure {
-	//method: push element
-	public static void pushElement(){
-		//object creation
+	// method: push element
+	public static void pushElement() {
+		// object creation
 		StackAndQueue<Integer> stack = new StackAndQueue<Integer>();
 		stack.push(70);
 		stack.push(30);
 		stack.push(56);
 		stack.print();
-		
+
+	}
+
+	// method: peek element
+	public static void peekElement() throws Exception {
+		// object creation
+		StackAndQueue<Integer> stack = new StackAndQueue<Integer>();
+		stack.push(70);
+		stack.push(30);
+		stack.push(56);
+		stack.peek();
+		System.out.println("------------------");
+	}
+
+	// method: peek element
+	public static void popElement() throws Exception {
+		// object creation
+		StackAndQueue<Integer> stack = new StackAndQueue<Integer>();
+		stack.push(70);
+		stack.push(30);
+		stack.push(56);
+		stack.pop();
+		stack.print();
 	}
 
 	public static void main(String[] args) throws Exception {
 		System.out.println("Welcome to Stack and Queue");
 		System.out.println("Push element in stack ");
 		pushElement();
-		
+
+		System.out.println("Peek element in stack ");
+		peekElement();
+
+		System.out.println("Pop element in stack ");
+		popElement();
 	}
 
 }
--- src/com/dataStructures/StackAndQueue.java
+++ src/com/dataStructures/StackAndQueue.java
@@ -1,35 +1,59 @@
 package com.dataStructures;
 
 public class StackAndQueue<E> {
-	//variable declaration
+	// variable declaration
 	Node<E> head;
 
-	//method: push element
+	// method: push element
 	public void push(E key) {
-		//create new node
+		// create new node
 		Node<E> newNode = new Node<E>(key);
-		//check stack is empty or not
+		// check stack is empty or not
 		if (isEmpty()) {
 			head = newNode;
 			return;
 		}
 		Node<E> temp = head;
-		while(temp.next != null) {
+		while (temp.next != null) {
 			temp = temp.next;
 		}
 		temp.next = newNode;
 	}
-	
-	//check stack is empty or not
+
+	// check stack is empty or not
 	public boolean isEmpty() {
 		return head == null;
 	}
-		
-	//method: print stack and queue
+
+	// get peek element
+	public void peek() throws Exception {
+		Node<E> temp = head;
+		if (temp == null) {
+			throw new Exception("cannot peek last element from empty stack");
+		}
+		while (temp.next != null) {
+			temp = temp.next;
+		}
+		System.out.println(temp.getKey());
+	}
+
+//pop element
+	public void pop() throws Exception {
+		if (isEmpty()) {
+			throw new Exception("popping from empty stack is not allowed");
+		}
+		Node<E> temp = head;
+		while (temp.next.next != null) {
+			temp = temp.next;
+		}
+		temp.next = null;
+	}
+
+	// method: print stack and queue
 	public void print() {
 		Node<E> temp = head;
-		while(temp.next != null) {
-			System.out.print(temp.getKey()+"->");
+		while (temp.next != null) {
+			System.out.print(temp.getKey() + "->");
 			temp = temp.next;
 		}
 		System.out.println(temp.getKey());
